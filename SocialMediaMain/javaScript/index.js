6696ee0f19f7bec2608ba8f11ac1ce6d3aa15e90
ALEJOO <zallesrene@gmail.com>
2024-03-07 11:37:21 -0400

    Proyecto con JavaScript

---
+++ SocialMediaMain/javaScript/index.js
@@ -0,0 +1,13 @@
+/* console.log("Hola mundo");
+function suma(x, y)
+{
+    let resultado;
+    resultado = x + y;
+    return resultado;
+} */
+
+let boton = document.querySelector("button");
+
+boton.addEventListener("click", function() {
+    console.log("Miguel gay");
+});
